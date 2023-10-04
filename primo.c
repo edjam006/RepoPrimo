5b9afb665d27af629ff346c0598a8596ae280b89
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-04 16:25:20 -0500

    Calculo de los divisores para saber si es primo mediante un bucle for

--- primo.c
+++ primo.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main (){
-    int num;
+    int num, i, prim = 1;
     printf("Ingrese un numero entero\n");
     scanf("%d", &num);
+    if (num <= 1) {
+        prim = 0;
+    } else {
+       
+        for (i = 2; i < num; i++) {
+            
+            if (num % i == 0) {
+                prim = 0;
+                
+            }
+        }
+    }
+    if (prim == 1){
+        printf("El numero es primo\n");
+    } else {
+        printf("El numero no es primo\n");
+    }
+
 }
